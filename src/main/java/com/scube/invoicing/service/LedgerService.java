08fa1256aa9063eee66764b511ba8cbc49a5f227
sarveshpandit18 <sarvesh.pandit1813@gmail.com>
2023-05-02 16:27:50 +0530

    Ledger service file

--- src/main/java/com/scube/invoicing/service/LedgerService.java
+++ src/main/java/com/scube/invoicing/service/LedgerService.java
@@ -1,9 +1,15 @@
 package com.scube.invoicing.service;
 
 import com.scube.invoicing.entity.CheckInvoiceMailStatusEntity;
+import com.scube.invoicing.entity.CustomerInvoiceEntity;
+import com.scube.invoicing.entity.ExpenseInfoEntity;
 
 public interface LedgerService {
 
 	boolean addLedgerEntryForInvoice(CheckInvoiceMailStatusEntity checkInvoiceMailStatusEntity);
+	
+	boolean updateLedgerEntryForInvoicePaymentReceived(CustomerInvoiceEntity customerInvoiceEntity);
+	
+	boolean addLedgerEntryForExpense(ExpenseInfoEntity expenseInfoEntity);
 	
 }
